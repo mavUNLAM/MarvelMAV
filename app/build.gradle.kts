90db2ade6c5b823019ef413612161ec7c2ea2d60
Marcos A. Verdura <mav.unlam@gmail.com>
2024-09-14 11:19:44 -0300

    agregados los archivos necesarios para los temas de compose. Agregado .gitignore. Agregado dependencias para la descarga de fuentes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.foundation.android)
     implementation(libs.androidx.material3.android)
     implementation(libs.androidx.ui.tooling.preview.android)
+    implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
